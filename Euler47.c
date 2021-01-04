2e70d731b943c6147fd9a14fef0d2760b664301b
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2021-01-04 11:28:32 +0530

    Solution to Euler 47

---
+++ Euler47.c
@@ -0,0 +1,68 @@
+/*
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 × 7
+15 = 3 × 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2² × 7 × 23
+645 = 3 × 5 × 43
+646 = 2 × 17 × 19.
+
+Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
+
+Author: https://github.com/ravikumark815
+
+Answer: 134043
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define N 1000000
+
+uint8_t number_prime_factor[N];
+
+/* init_NPF: initialize the Number of Prime Factor (NPF) array. */
+void init_NPF(void)
+{
+	uint32_t i, j;
+
+	/* similar to Eratosthene sieve. Initialize all numbers with 0 prime
+	 * factors counter. Goes from 2 to N. If the current number has 0 prime
+	 * factor, it means it's a prime, so increment the counter for all
+	 * multiples of this prime (e.g. 2 is prime so every even number has
+	 * at least 2 as a prime factor).
+	 */
+	for (i = 0; i < N; ++i)
+		number_prime_factor[i] = 0;
+
+	for (i = 2; i < N; ++i)
+		if (number_prime_factor[i] == 0)
+			for (j = 2*i; j < N; j += i)
+				++number_prime_factor[j];
+}
+
+/* find the first 4 consecutive integers that have 4 different prime factors */
+int32_t main(void)
+{
+	int32_t i;
+
+	init_NPF();
+	for (i = 0; i < N-4; ++i)
+	{
+		if (number_prime_factor[i] != 4)
+			continue;
+		if (number_prime_factor[i+1] != 4)
+			continue;
+		if (number_prime_factor[i+2] != 4)
+			continue;
+		if (number_prime_factor[i+3] != 4)
+			continue;
+		break;
+	}
+
+	printf("Problem 47: %d\n", i);
+	return 0;
+}
